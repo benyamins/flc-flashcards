51c2dc64377c4867f95101e3781b18801fc70593
Benyamins <benjarq@hotmail.com>
2023-01-13 17:30:48 -0300

    adding imgui

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 set_project("flc-flashcards")
 add_rules("mode.debug", "mode.release")
 
-add_requires("fmt", "nlohmann_json")
+add_requires("fmt", "nlohmann_json", "sdl2", "opengl")
 
 if is_mode("debug") then
 	add_cxxflags("-std=c++2b", "-Wall", "-Werror", "-pedantic", "-pedantic-errors", "-Wconversion")
@@ -11,6 +11,12 @@ end
 
 target("flc")
 	set_kind("binary")
-	add_files("src/*.cpp")
 	add_includedirs("include")
-	add_packages("fmt", "nlohmann_json")
+	add_includedirs("external/imgui")
+	add_includedirs("external/imgui/backends")
+	add_files("external/imgui/backends")
+    add_files("external/imgui/*.cpp")
+    add_files("external/imgui/backends/imgui_impl_sdl.cpp")
+    add_files("external/imgui/backends/imgui_impl_opengl3.cpp")
+	add_packages("fmt", "nlohmann_json", "sdl2", "opengl")
+	add_files("src/*.cpp")
