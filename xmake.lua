8889d8113a999b0aa0218bd378f36259c91bc2dc
benyamins <benjarq@hotmail.com>
2023-01-15 12:34:09 -0300

    separating imgui from targer for eliminating warnings from deps

--- xmake.lua
+++ xmake.lua
@@ -4,18 +4,24 @@ add_rules("mode.debug", "mode.release")
 add_requires("fmt", "nlohmann_json", "sdl2", "opengl")
 
 if is_mode("debug") then
-	add_cxxflags("-std=c++2b", "-Wall", "-pedantic", "-pedantic-errors", "-Wconversion")
+	add_cxxflags("-std=c++2b", "-Wall", "-pedantic", "-pedantic-errors", "-Wconversion", "-Werror")
 else
 	add_cxxflags("-std=c++2b", "-Wall", "-Werror", "-pedantic", "-pedantic-errors", "-Wconversion", "-Wextra")
 end
 
-target("flc")
-	set_kind("binary")
-	add_includedirs("include")
-	add_includedirs("external/imgui")
-	add_includedirs("external/imgui/backends")
+target("imgui")
+    --set_kind("static")
+    set_warnings("none")
+	add_packages("sdl2", "opengl")
+	add_includedirs("external/imgui", {public = true})
+	add_includedirs("external/imgui/backends", {public = true})
     add_files("external/imgui/*.cpp")
     add_files("external/imgui/backends/imgui_impl_sdl.cpp")
     add_files("external/imgui/backends/imgui_impl_opengl3.cpp")
+
+target("flc")
+	set_kind("binary")
+	add_includedirs("include")
 	add_packages("fmt", "nlohmann_json", "sdl2", "opengl")
 	add_files("src/*.cpp")
+	add_deps("imgui")
